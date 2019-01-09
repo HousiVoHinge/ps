8a27081fce4d95d760e34fe600f6f3906d425f12
HousiVoHinge <housi.hingerembaerg@gmail.com>
2019-01-09 20:12:18 +0100

    Added comments

--- ps.c
+++ ps.c
@@ -6,26 +6,30 @@
 #include <ctype.h>
 #include <unistd.h>
 
-void printFileProperties(char *fileName);
+void printProcessProperties(char *processID);
 
-void removeAllChars(char *str, char c);
+void removeChars(char *string, char c);
 
-int readFilesFromDir(char* dir);
+int readFilesFromDir(char* dirPath);
 
 int main() {
     readFilesFromDir("/proc");
 }
 
-void printFileProperties(char *fileName) {
+/**
+ * Reads the status file of the given process and prints its ID, name and size.
+ * @param processID: Print properties of this process
+*/
+void printProcessProperties(char *processID) {
     FILE *filePtr;
     char *linePtr = NULL;
     size_t length = 0;
 
+    // Open the status file of the process
     char filePath[255];
     strcpy(filePath, "/proc/");
-    strcat(filePath, fileName);
+    strcat(filePath, processID);
     strcat(filePath, "/status");
-
     filePtr = fopen(filePath, "r");
 
     // Return when directory has no status file
@@ -35,7 +39,8 @@ void printFileProperties(char *fileName) {
 
     char processName[255];
     char processUID[255];
-    char processSize[255];
+    char processSize[255] = "0";
+
     // Loop over all lines of the status file and extract wanted data
     while ((getline(&linePtr, &length, filePtr)) != -1) {
         if (strstr(linePtr, "Name") != NULL) {
@@ -58,26 +63,32 @@ void printFileProperties(char *fileName) {
     }
 
     // Remove unnecessary chars for output
-    removeAllChars(processName, ' ');
-    removeAllChars(processName, '\n');
-    removeAllChars(processName, '\t');
+    removeChars(processName, ' ');
+    removeChars(processName, '\n');
+    removeChars(processName, '\t');
 
-    removeAllChars(processSize, ' ');
-    removeAllChars(processSize, '\n');
-    removeAllChars(processSize, '\t');
-    removeAllChars(processSize, 'k');
-    removeAllChars(processSize, 'B');
+    removeChars(processSize, ' ');
+    removeChars(processSize, '\n');
+    removeChars(processSize, '\t');
+    removeChars(processSize, 'k');
+    removeChars(processSize, 'B');
 
-
-    printf("%s %s %s\n", fileName, processName, processSize);
+    // Print final output
+    printf("%s %s %s\n", processID, processName, processSize);
 
     if (linePtr) {
         free(linePtr);
     }
 }
 
-void removeAllChars(char *str, char c) {
-    char *pr = str, *pw = str;
+/**
+ * Removes all occurrences of the given character from the string.
+ * @param string: String which is shortened
+ * @param c: Character to remove
+*/
+void removeChars(char *string, char c) {
+    char *pr = string;
+    char *pw = string;
     while (*pr) {
         *pw = *pr++;
         pw += (*pw != c);
@@ -85,12 +96,16 @@ void removeAllChars(char *str, char c) {
     *pw = '\0';
 }
 
-int readFilesFromDir(char* dir) {
+/**
+ * Reads all files from a directory.
+ * @param dirPath: Read files from the directory at this path
+*/
+int readFilesFromDir(char* dirPath) {
     DIR *directory;
     struct dirent *subFolder;
-    directory = opendir(dir);
+    directory = opendir(dirPath);
     while ((subFolder = readdir(directory)) != NULL) {
-        printFileProperties(subFolder->d_name);
+        printProcessProperties(subFolder->d_name);
     }
     closedir(directory);
 }
